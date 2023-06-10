a317d1b7ea6f9037bbac1970899bbd04f98b5138
souhailas142 <souhailas142@gmail.com>
2023-06-10 11:05:47 -0700

    .

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    firstC = sentence[0] if length > 1 else "None"
+    firstC = sentence[0] if length >= 1 else "None"
     return (length, firstC)
