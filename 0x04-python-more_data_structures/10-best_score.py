e112720a167dc62cf7d1c1552fa89b93f51bb9a7
souhailas142 <souhailas142@gmail.com>
2023-06-14 14:43:38 -0700

    .

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
+    if a_dictionary:
+        best = next(iter(a_dictionary))
+        max_sc = a_dictionary.get(best)
+        for key in a_dictionary:
+            if a_dictionary[key] > max_sc:
+                max_sc = a_dictionary[key]
+                best = key
+        return best
+    else:
+        return "None"
