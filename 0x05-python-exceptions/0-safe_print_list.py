99505dee19545292ae4153939504bca98338c92e
souhailas142 <souhailas142@gmail.com>
2023-06-26 11:35:05 -0700

    .

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         _len += 1
     for index, item in enumerate(my_list):
         if x == index:
-            break;
+            break
         print(item, end="")
     print()
     return (_len if (x > _len) else x)
